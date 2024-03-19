260c622cf645405023a8c232d0db79ae4cc964c5
Kelli Miller <49914948+MillerITEd@users.noreply.github.com>
2024-03-19 17:29:41 -0500

    Print "Meanwhile..."

--- CSC160Spring2024/src/csc160spring2024/CSC160Spring2024.java
+++ CSC160Spring2024/src/csc160spring2024/CSC160Spring2024.java
@@ -15,6 +15,7 @@ public class CSC160Spring2024 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Meanwhile...");
     }
     
 }
